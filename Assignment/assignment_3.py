341563211b4e22b1029d22bd2ebc8bb6a27e0e6c
meenThararat <thararat.s@rmutsvmail.com>
2022-08-26 20:27:56 +0700

    Add assignment

---
+++ Assignment/assignment_3.py
@@ -0,0 +1,31 @@
+"""
+Name: {Thararat Sakweth}
+SID: {364211760011}
+Group: {MIT221}
+"""
+
+"""
+เขียนโปรแกรมไพธอนเพื่อนำเลขบัตรประชาชนมาบวกกันเพื่อทำนายดวงชะตาดังนี้
+• สร้างฟังก์ชันเพื่อหาผลรวมของเลขบัตรประชาชน โดยใช้ชื่อฟังก์ชั่นว่า sumPID()
+• สร้างฟังชั่นชื่อ getFortune() เพื่อทำนายดวงชะตา ถ้าเป็นเลขคู่ให้ทำนายว่า your fortune is good
+• ถ้าเป็นเลขคี่ ให้ทำนายว่า your fortune is very good
+
+ใช้ฟังก์ชั่น split()
+"""
+
+def getPTD():
+    sumPID = [int(x) for x in input(f'กรุณากรอกเลขบัตรประชาชน : ')]
+    return sumPID
+
+def getFortune(*var):
+    resultlist = []
+    for x in var:
+        if x % 2 == 0:
+            resultlist.append('Your fortune is good')
+        else:
+            resultlist.append('Your fortune is very good')
+    return resultlist
+
+PID = getPTD()
+print(f'ผลการคำนวณเลขบัตรประชาชนของคุณ: {sum(PID)}')
+print(f'ผลการทำนาย: {getFortune(sum(PID))}')
